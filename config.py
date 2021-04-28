16e613db71d638c407755622616330194ab56a3d
deba10106 <deba10106@gmail.com>
2021-04-28 20:14:14 +0800

    networking fixed

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class ProductionConfig(Config):
     DEBUG=False
     UPLOAD_FOLDER = 'UPLOADS'
     SECRET_KEY = 'yhjfyvjuyetcuyfuyjvkutgkigukjgbkg'
-    DB_HOST = 'localhost'
+    DB_HOST = '0.0.0.0'
     DB_NAME = 'postgres'
     DB_USER = 'postgres'
     DB_PASS = 'mysecretpassword'
